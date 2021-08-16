f717f3f8d33388ad8059c88b6201ca4c01c36f41
gmul <greg@krystaltek.com>
2021-08-16 12:00:14 +1000

    added html

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
+from django import http
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpResponse("Hello you!")
+    return render(request, 'hello.html') 
