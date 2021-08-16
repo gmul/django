ad6a46b7f095ce526562b772c1e177df0f32b910
gmul <greg@krystaltek.com>
2021-08-16 17:48:54 +1000

    added django toolbar

--- storefront/urls.py
+++ storefront/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('playground/', include('playground.urls'))
+     path('playground/', include('playground.urls')),
+     path('__debug__/', include(debug_toolbar.urls)),
 ]
