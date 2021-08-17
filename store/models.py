df2b7daea33bcd8abcdd301a8eaf44113e3fc644
gmul <greg@krystaltek.com>
2021-08-17 13:50:59 +1000

    added class relationships

--- store/models.py
+++ store/models.py
@@ -1,35 +1,53 @@
+from typing import Collection
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE, PROTECT, SET_NULL
 from django.db.models.enums import Choices
-from django.db.models.fields import CharField
+from django.db.models.fields import CharField, DecimalField, PositiveIntegerField
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
+
+# many to many - Promotion - Products
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Products', on_delete=models.SET_NULL, null=True, related_name='+')
+
 class Products(models.Model):
     MEMBERSHIP_BRONZE = 'B'
     MEMBERSHIP_SILVER = 'S'
     MEMBERSHIP_GOLD = 'G'
 
 
-     MEMBERSHIP_CHOICES = [
+    MEMBERSHIP_CHOICES = [
         (MEMBERSHIP_BRONZE, 'Bronze'),
         (MEMBERSHIP_SILVER, 'Silver'),
         (MEMBERSHIP_GOLD, 'Gold'),
-     ]
+    ]
 
      #sku = models.CharField(max_length=255, primary_key=True)
-     title = models.CharField(max_length=255)   #varchar(255)
-     description = models.TextField()
-     price =models.DecimalField(max_digits=6,decimal_places=2)
-     inventory = models.IntegerField()
-     last_update = models.DateTimeField(auto_now=True)
-     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    title = models.CharField(max_length=255)   #varchar(255)
+    description = models.TextField()
+    price =models.DecimalField(max_digits=6,decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
-class Customer(models.Model):
+class Customers(models.Model):
     first_name =  models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255,unique=True)
     phone = models.CharField(max_length=30)
     birth_date = models.DateField(null=True)
+   
 
 class Order(models.Model):
     PENDING = 'P'
@@ -44,3 +62,25 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=255, choices=PAYMENT_STATUS, default=PENDING)
+    customer = models.ForeignKey(Customers, on_delete=models.PROTECT)
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customers, on_delete=CASCADE)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Products, on_delete=models.PROTECT)
+    quantity = models.PositiveIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+  cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+  product = models.ForeignKey(Products, on_delete=models.CASCADE)  
+  quantity = models.PositiveSmallIntegerField()
+
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE
+from django.db.models.manager import Manager
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+class TaggedItem(models.Model):
+    # What tag is applidd to what object
+    tag = models.ForeignKey(Tag, on_delete=CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=CASCADE)
+    object_id = models.PositiveIntegerField()
+    object_type = (GenericForeignKey)
+
+
+                          
